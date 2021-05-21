330c196ed9df3246afdcc79a4de44bf7accf9cc3
kellraym <83917084+kellraym@users.noreply.github.com>
2021-05-20 20:59:41 -0600

    Add Computer class

--- src/computer.js
+++ src/computer.js
@@ -1,5 +1,21 @@
 class Computer {
   
+  constructor(name, operatingSystem, processor, memory, graphics) {
+    this.name = name,
+    this.operatingSystem = operatingSystem,
+    this.processor = processor,
+    this.memory = memory,
+    this.graphics = graphics,
+    this.on = false
+  }
+
+  power() {
+    if (this.on === false) {
+      this.on = true;
+    } else {
+      this.on = false;
+    }
+  }
 }
 
 module.exports = Computer
