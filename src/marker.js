ea420adceb81bc14531da9a449147bd20af816d5
kellraym <83917084+kellraym@users.noreply.github.com>
2021-05-20 21:01:32 -0600

    Add Marker class

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,19 @@
 class Marker {
-  
+  constructor(size, color, remainingInk) {
+    this.size = size,
+      this.color = color,
+      this.remainingInk = remainingInk
+  }
+
+  write(string) {
+    let noSpaces = string.split(" ").join("");
+
+    let difference = (noSpaces.length - this.remainingInk);
+    
+    this.remainingInk -= noSpaces.length;
+ 
+    return string.substring(0, (string.length - difference));
+  }
 }
 
 module.exports = Marker
